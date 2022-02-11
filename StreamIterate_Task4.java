0ceb7f4000d4ce41df7999134aa138c065471cfd
nastialsk <95052689+nastialsk@users.noreply.github.com>
2022-02-11 17:26:01 +0200

    Update StreamIterate_Task4.java

--- StreamIterate_Task4.java
+++ StreamIterate_Task4.java
@@ -1,1 +1,13 @@
-1
+import java.util.stream.Stream;
+
+class RandomInfinity {
+    public void GetInfinity() {
+        int a = 25214903917;
+        int c = 11;
+        int m = (int) Math.pow(2, 48);
+        for (int n = 0; n <= m; n++) {
+            int[] x = new int[n];
+            System.out.println(Stream.iterate(x[0], x[n + 1] = (a * x[n] + c) % m));
+        }
+    }
+}
